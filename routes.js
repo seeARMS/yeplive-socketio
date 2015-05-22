2bb0b8011faa491b89afe52548178cd66a41f04e
Evan Marsland <ERmarsland@gmail.com>
2015-05-22 09:06:29 -0400

    a line

--- routes.js
+++ routes.js
@@ -342,9 +342,9 @@ module.exports = function(app, io){
 		socket.on('leave_room', function(data){
 			chat.removeUser(socket.yep_id, socket.user_id, function(){
 				chat.getUsers(socket.yep_id, function(err, res){
-				io.to(socket.yep_id).emit('chat:users', res);
-				});
+					io.to(socket.yep_id).emit('chat:users', res);
 					socket.leave(socket.yep_id);
+				});
 			});
 		});
 
